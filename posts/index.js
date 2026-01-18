136cbdbfd8264ae60e748fcb99c147b8017ee47e
lueezzzz <rianmabait45@gmail.com>
2026-01-19 03:54:38 +0800

    posts service

--- posts/index.js
+++ posts/index.js
@@ -1,3 +1,36 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser')
+const {randomBytes} = require('crypto');
+
+
 const app = express();
+app.use(bodyParser.json());
+
+const posts = {
+    
+}
+
+app.get('/', (request, response)=>{
+    response.send('hello')
+})
+
+app.get('/posts', (request, response)=>{
+    response.send(posts);
+})
+
+app.post('/posts', (request, response)=>{
+    const id = randomBytes(4).toString('hex');
+    const {title} = request.body;
+    posts[id] = {
+        id, title
+    }
+    response.status(201).send(posts[id]);
+})
+
+app.listen(4000, ()=>{
+    console.log("test server");
+})
+
+
+
 
